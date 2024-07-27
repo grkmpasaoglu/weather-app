d61ef2c87d0af073bf15eec8677c238044ec37d8
grkmpasaoglu <gorkempasaoglu@outlook.com>
2024-07-28 00:09:48 +0300

    date and hour added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { WeatherProvider } from "./context/WeatherContext";
 import Weather from "./components/Weather";
 
 function App() {
-  return (
-    <WeatherProvider>
-      <Weather />
-    </WeatherProvider>
-  );
+    return (
+        <WeatherProvider>
+            <Weather />
+        </WeatherProvider>
+    );
 }
 
 export default App;
